021d5d8514b43e88e6d7e5636608ac69ad9a86b2
Thomas ROUSSELIN <t.rousselin@mtarget.fr>
2025-08-19 12:43:41 +0200

    init project

--- main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class VideoGame {
-    label = '';
-}
-const videoGame = new VideoGame();
-//# sourceMappingURL=main.js.map
