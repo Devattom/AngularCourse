ea510c018d2d437336c61927b73b246a8f155c51
thomas Rousselin <devattom@gmail.com>
2025-09-11 14:15:48 +0200

    complete section 12

--- org/apps/angular-course/src/app/app.ts
+++ org/apps/angular-course/src/app/app.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import {MatSliderModule} from '@angular/material/slider';
 import {RouterOutlet} from "@angular/router";
 import {MainMenu} from "../shared/menu/main-menu/main-menu";
+import { ComputePipe } from '../shared/tools/compute-pipe';
+import { AutoFocus } from '../shared/tools/auto-focus';
 
 @Component({
-  imports: [RouterOutlet, MatSliderModule, MainMenu],
+  imports: [RouterOutlet, MatSliderModule, MainMenu, ComputePipe, AutoFocus],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'angular-course';
+  protected value = 5;
 }
